095b26c159c15838d0fc5ae73ca576867822622e
rerolf <64596093+rerolf@users.noreply.github.com>
2020-06-14 14:51:09 +0300

    Update eduard.java

--- eduard.java
+++ eduard.java
@@ -1,32 +1,38 @@
+package first;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class test {
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Введите длинну массива: ");
 		int N = in.nextInt();
-		int[] X = new int[N];
+		double[] X = new double[N];
 		System.out.println("Введите элементы массива:");
 		for (int i = 0; i < N; i++) {
 			System.out.print("X" + i + ": ");
-			X[i] = (int) in.nextDouble();
+			X[i] =  in.nextDouble();
 		}
 		in.close();
-		int minEven=0;
+		double minEven=0;
 		for(int iterator=0;iterator<N;iterator++) {
-			if(Math.abs(X[iterator])%2==0) {
-				minEven=iterator;
+			if(Math.abs(X[iterator])%2.0==0.0) {
+				minEven=X[iterator];
 				break;
 			}
 		}
 		int counterOdd=0;
 		for (int iterator=0;iterator<N;iterator++) {
-			if(Math.abs(X[iterator])%2==1) counterOdd++;
-			if((Math.abs(X[iterator])%2==0)&&(X[iterator]<=X[minEven])) minEven = iterator;
+			if(Math.abs(X[iterator])%1.0!=0.0) counterOdd++;
+			if((Math.abs(X[iterator])%2.0==0.0)&&(X[iterator]<minEven)) minEven = X[iterator];
 		}
 		System.out.println(counterOdd);
 		if(counterOdd!=0) {
-			int[] lessArray = new int[counterOdd];
+			double[] lessArray = new double[counterOdd];
 			int lengthCounter =0;
 			for(int iterator=0;iterator<X.length;iterator++) {
-				if(Math.abs(X[iterator])%2==1) {
+				if(Math.abs(X[iterator])%1.0!=0.0) {
 					lessArray[lengthCounter]=X[iterator];
 					lengthCounter++;
 				}
@@ -35,9 +41,11 @@
 				System.out.println("Элемент нового массива ["+iterator+"] = "+lessArray[iterator]);
 			}
 		}
-		if (Math.abs(X[minEven])%2 == 0) {
-			System.out.println("\nМинимальный четный элемент: "+"["+minEven+"]" + X[minEven]);
+		if (Math.abs(minEven)%2 == 0.0) {
+			System.out.println("\nМинимальный четный элемент: "+minEven);
 		} else {
 			System.out.println("Чётные элементы отсутствуют");
 		}
 	}
+
+}
