996adf973cc1e9ffe00e33fbb858649f93196c2a
rerolf <64596093+rerolf@users.noreply.github.com>
2020-06-14 12:41:49 +0300

    Update and rename eduard to eduard.java

--- eduard.java
+++ eduard.java
@@ -11,22 +11,22 @@
 		in.close();
 		int minEven=0;
 		for(int iterator=0;iterator<N;iterator++) {
-			if(X[iterator]%2==0) {
+			if(Math.abs(X[iterator])%2==0) {
 				minEven=iterator;
 				break;
 			}
 		}
 		int counterOdd=0;
 		for (int iterator=0;iterator<N;iterator++) {
-			if(X[iterator]%2==1) counterOdd++;
-			if((X[iterator]%2==0)&&(X[iterator]<=X[minEven])) minEven = iterator;
+			if(Math.abs(X[iterator])%2==1) counterOdd++;
+			if((Math.abs(X[iterator])%2==0)&&(X[iterator]<=X[minEven])) minEven = iterator;
 		}
 		System.out.println(counterOdd);
 		if(counterOdd!=0) {
 			int[] lessArray = new int[counterOdd];
 			int lengthCounter =0;
 			for(int iterator=0;iterator<X.length;iterator++) {
-				if(X[iterator]%2==1) {
+				if(Math.abs(X[iterator])%2==1) {
 					lessArray[lengthCounter]=X[iterator];
 					lengthCounter++;
 				}
@@ -35,7 +35,9 @@
 				System.out.println("Элемент нового массива ["+iterator+"] = "+lessArray[iterator]);
 			}
 		}
-		if (X[minEven]%2 == 0) {
+		if (Math.abs(X[minEven])%2 == 0) {
 			System.out.println("\nМинимальный четный элемент: "+"["+minEven+"]" + X[minEven]);
-		} else System.out.println("Чётные элементы отсутствуют");
+		} else {
+			System.out.println("Чётные элементы отсутствуют");
 		}
+	}
